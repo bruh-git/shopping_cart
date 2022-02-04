46e24563268fbae360fd0544e8beab9389e06f72
Bruna <brunac.cardososilva@gmail.com>
2022-02-03 21:14:07 -0300

    Requisito 11
    Em desenvolvimento
    
    Concluido.

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,5 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-
+  it('se, ao executar getSavedCartItems, o método localStorage.getItem é chamado,',() => {
+    getSavedCartItems();
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+  it('se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro.',() => {
+    getSavedCartItems();
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems');
+  })
 });
